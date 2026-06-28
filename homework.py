a0be61d06c0bb32bf70e01fecdb0d46dc06031dd
anikaparekh2011 <anikaparekh2011@gmail.com>
2026-06-28 15:11:09 +0100

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,8 @@
+a = int(input("enter first number: "))
+b = int(input("enter first number: "))
+c = int(input("enter first number: "))
+print("This is the order of numbers: ", a , b , c)
+print("Now I wil swap all the numbers!! ")
+
+a, b, c = b , c , a
+print("New order:", a, b, c,  )
